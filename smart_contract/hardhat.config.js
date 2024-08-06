9050e32036b2e7ce61d3dbe3c4ff5e79bc2a8c9d
Harry <105970544+h-s14@users.noreply.github.com>
2024-08-07 02:46:58 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/6AMOD5PtY_8c7vA8KdCxnoo9-x1_ywRp",
       accounts: [
-        "fde09cd6f2f1ed20d604df0ddbd43901aa9a49a17ca8bd13d20860289b92d47f",
+        "//Enter the private address of the wallet you want to provide GAS from to deploy the contract",
       ],
     },
   },
